2ea1fc122226af50a423eec3d98599337228e289
Alexander Kim <>
2021-04-29 22:53:22 -0400

    init

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+
+df = pd.read_csv('vehicles_us.csv')
+df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
+
+st.header('Data viewer')
+show_manuf_1k_ads = st.checkbox('Include manufacturers with less than 1000 ads')
+if not show_manuf_1k_ads:
+    df = df.groupby('manufacturer').filter(lambda x: len(x) > 1000)
+
+st.dataframe(df)
+st.header('Vehicle types by manufacturer')
+st.write(px.histogram(df, x='manufacturer', color='type'))
+st.header('Histogram of `condition` column vs `model_year`')
+year_min = st.number_input('Enter minimum value for the year to be displayed',
+                           value=1990,
+                           min_value=int(df['model_year'].min()),
+                           max_value=int(df['model_year'].max()))
+st.write(px.histogram(df[df['model_year'] >= year_min], x='model_year', color='condition'))
+
+
+st.header('Compare price distribution between manufacturers')
+manufac_list = sorted(df['manufacturer'].unique())
+manufacturer_1 = st.selectbox('Select manufacturer 1',
+                              manufac_list, index=manufac_list.index('chevrolet'))
+
+manufacturer_2 = st.selectbox('Select manufacturer 2',
+                              manufac_list, index=manufac_list.index('hyundai'))
+mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
+df_filtered = df[mask_filter]
+normalize = st.checkbox('Normalize histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+st.write(px.histogram(df_filtered,
+                      x='price',
+                      nbins=30,
+                      color='manufacturer',
+                      histnorm=histnorm,
+                      barmode='overlay'))
